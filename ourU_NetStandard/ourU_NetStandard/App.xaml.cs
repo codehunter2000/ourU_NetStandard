2ef45f873126023d924258a9bc58c54dd0fe204a
codehunter2000 <gabrielrhunt@gmail.com>
2018-10-31 16:46:09 -0700

    Initial commit.

---
+++ ourU_NetStandard/ourU_NetStandard/App.xaml.cs
@@ -0,0 +1,68 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using System;
+using ourU_NetStandard.Services;
+using ourU_NetStandard.Views;
+using Microsoft.Identity.Client;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.MobileServices;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace ourU_NetStandard
+{
+    public partial class App : Application
+    {
+
+        public interface IAuthenticate
+        {
+            Task<bool> Authenticate();
+        }
+
+        public static IAuthenticate Authenticator { get; private set; }
+        public static IAuthenticate AuthenticationProvider { get; private set; }
+        public static string AzureBackendUrl = "https://ouru.azurewebsites.net";    
+        public static MobileServiceClient MobileService = new MobileServiceClient(AzureBackendUrl);
+
+
+        public static void Init(IAuthenticate authenticator)
+        {
+            Authenticator = authenticator;
+        }
+
+        public App()
+        {
+            InitializeComponent();
+            MainPage = new LogInPage();
+        }
+
+        protected override void OnActivated(IActivatedEventArgs args)
+        {
+            if (args.Kind == ActivationKind.Protocol)
+            {
+                ProtocolActivatedEventArgs protocolArgs = args as ProtocolActivatedEventArgs;
+                Frame content = Window.Current.Content as Frame;
+                if (content.Content.GetType() == typeof(MainPage))
+                {
+                    content.Navigate(typeof(MainPage), protocolArgs.Uri);
+                }
+            }
+            Window.Current.Activate();
+            base.OnActivated(args);
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ ourU_NetStandard/ourU_NetStandard/Views/LogInPage.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Identity.Client;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Microsoft.WindowsAzure.MobileServices;
+using Newtonsoft.Json.Linq;
+
+namespace ourU_NetStandard.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class LogInPage : ContentPage
+	{
+
+        // Track whether the user has authenticated.
+        bool authenticated = false;
+        private MobileServiceUser user;
+
+        public LogInPage()
+        {
+            InitializeComponent();
+        }
+
+        // Define a member variable for storing the signed-in user. 
+        
+
+        // Define a method that performs the authentication process
+        // using a Facebook sign-in. 
+        private async System.Threading.Tasks.Task<bool> AuthenticateAsync()
+        {
+            string message;
+            bool success = false;
+            try
+            {
+                // Change 'MobileService' to the name of your MobileServiceClient instance.
+                // Sign-in using Facebook authentication.
+                user = await App.MobileService
+                    .LoginAsync(MobileServiceAuthenticationProvider.Google, JObject.Parse(App.AzureBackendUrl));
+                message =
+                    string.Format("You are now signed in - {0}", user.UserId);
+
+                success = true;
+            }
+            catch (InvalidOperationException)
+            {
+                message = "You must log in. Login Required";
+            }
+            return success;
+        }
+
+        private async void ButtonLogin_Click(object sender, RoutedEventArgs e)
+        {
+            // Login the user and then load data from the mobile app.
+            if (await AuthenticateAsync())
+            {
+                // Switch the buttons and load items from the mobile app.
+                ButtonLogin.Visibility = Visibility.Collapsed;
+                ButtonSave.Visibility = Visibility.Visible;
+                //await InitLocalStoreAsync(); //offline sync support.
+                await RefreshTodoItems();
+            }
+        }
+    }
+}
