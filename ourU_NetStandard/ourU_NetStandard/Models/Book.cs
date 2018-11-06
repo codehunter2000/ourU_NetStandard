e0c8e99ebd89e1166dde49ec011b04c3179c9c57
Gabriel <hunt043@cougars.csusm.edu>
2018-11-05 16:49:04 -0800

    Added initial database files (not tested)

---
+++ ourU_NetStandard/ourU_NetStandard/Models/Book.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace ourU_NetStandard.Models
+{
+    public class Book : ContentPage
+    {
+
+        [Newtonsoft.Json.JsonProperty("Id")]
+        public string theISBN;
+
+        [Microsoft.WindowsAzure.MobileServices.Version]
+        public string AzureVersion { get; set; }
+
+        public string theAuthor;
+
+        public string theTitle;
+
+        public string thePrice;
+
+        public string theStatus;
+
+        public string theEdition;
+
+
+    }
+}
+
---
+++ ourU_NetStandard/ourU_NetStandard/Services/AzureMobileService.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.MobileServices;
+using Microsoft.WindowsAzure.MobileServices.SQLiteStore;
+using Microsoft.WindowsAzure.MobileServices.Sync;
+using Xamarin.Forms;
+
+namespace ourU_NetStandard.Services
+{
+    public class AzureMobileService
+    {
+        public MobileServiceClient MobileService { get; set; }
+        IMobileServiceSyncTable<Models.Book> bookTable;
+
+        public async Task Initialize()
+        {
+            MobileService = new MobileServiceClient("https://ouru.azurewebsites.net");
+            const string path = "books.db";
+            var store = new MobileServiceSQLiteStore(path);
+            store.DefineTable<Models.Book>();
+            await MobileService.SyncContext.InitializeAsync(store, new MobileServiceSyncHandler());
+        }
+
+        public async Task<IEnumerable> GetBooks()
+        {
+            await SyncBook();
+            return await bookTable.OrderBy(c => c.theISBN).ToEnumerableAsync();
+        }
+
+        public async Task AddBook(string isbn, string author, string title,
+                                 string price, string status, string edition)
+        {
+            var toAdd = new Models.Book
+            {
+                theISBN = isbn,
+                theAuthor = author,
+                theTitle = title,
+                thePrice = price,
+                theStatus = status,
+                theEdition = edition
+            };
+
+            await bookTable.InsertAsync(toAdd);
+
+            await SyncBook();
+
+        }
+
+        public async Task SyncBook()
+        {
+            await bookTable.PullAsync("allBooks", bookTable.CreateQuery());
+            await MobileService.SyncContext.PushAsync();
+        }
+    }
+}
+
