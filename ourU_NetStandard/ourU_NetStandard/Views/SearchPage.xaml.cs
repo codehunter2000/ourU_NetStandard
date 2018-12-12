c8b15530456fd82c8ba418eb38f9088680d1d440
kakon002 <kakon002@cougars.csusm.edu>
2018-12-12 13:25:54 -0800

    Jared Changes

--- ourU_NetStandard/ourU_NetStandard/Views/SearchPage.xaml.cs
+++ ourU_NetStandard/ourU_NetStandard/Views/SearchPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,10 +13,18 @@ namespace ourU_NetStandard.Views
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class SearchPage : ContentPage
 	{
-       
+        public Services.AzureMobileService azServ = new Services.AzureMobileService();
+        public ObservableCollection<Models.Book> bookList = new ObservableCollection<Models.Book>();
+
         public SearchPage ()
 		{
 			InitializeComponent ();
+            BookListView.ItemsSource = bookList;
+            azServ = new Services.AzureMobileService();
+            var task1 = Task.Run(
+            async () => await azServ.Initialize());
+            task1.Wait();
+            azServ.getBookCollection(ref bookList);
 		}
         async void BookItem_Clicked(object sender, System.EventArgs e)
         {
